0534176316e508a47e51c522acee778627f32f9e
airton <airtonfrance@gmail.com>
2019-07-03 15:00:41 -0400

    segundo commit

--- content/relatorio.php
+++ content/relatorio.php
@@ -1,82 +1,43 @@
-<?php 
+<?php
 
-	require_once __DIR__ . '/vendor/autoload.php';
-	
-	$sql_code = "SELECT a.nome AS alunonome, a.id_curso, c.nome AS cursonome, c.id_curso, c.id_professor, p.nome AS professornome, p.id_professor FROM aluno a
-		INNER JOIN curso c ON a.id_curso = c.id_curso 
-		INNER JOIN professor p ON c.id_professor = p.id_professor";
+  require_once __DIR__ . '/vendor/autoload.php';
 
-	$sql_query = $mysqli->query($sql_code) or die ($mysqli->error);
-	while($linha = $sql_query->fetch_assoc()){
+  $sql_code = "SELECT a.nome AS alunonome, a.id_curso, c.nome AS cursonome, c.id_curso, c.id_professor, p.nome AS professornome, p.id_professor FROM aluno a
+    INNER JOIN curso c ON a.id_curso = c.id_curso 
+    INNER JOIN professor p ON c.id_professor = p.id_professor";
 
-			echo $linha['alunonome'];
-			echo $linha['cursonome'];
-			echo $linha['professornome'];
-		}
+  $sql_query = $mysqli->query($sql_code) or die ($mysqli->error);
+  $linha = $sql_query->fetch_assoc();
 
-	$pagina ="
-<html>
-<body>
-<h1>Vintage Locadora</h1>
-<h2>Histórico de Locações de Clientes</h2>
+  $html='<h1>PDF</h1>
 
-<h3>Dados do Cliente</h3>
+  <table cellpadding="3" >
+    <tr bgcolor="#436EEE">
+      <td>Aluno</td>
+      <td>Curso</td>
+      <td>Professor</td>
+      
+      
+    </tr>
+    <?php
+    do{
+    ?>
+    <tr>
+      <td> '.$linha['alunonome'].'</td> 
+      <td> '.$linha['cursonome'].'</td>
+      <td> '.$linha['professornome'].'</td>
+      
+    </tr>
+    <?php 
+      }while($linha = $sql_query->fetch_assoc());
+    ?>
+  </table>';
+  error_reporting(E_ALL);
+  $mpdf=new Mpdf\Mpdf(); 
+  $mpdf->SetDisplayMode('fullpage');
+  $mpdf->WriteHTML($html);
+  $mpdf->Output();
 
-<table>
-  <tr>
-    <th>Nome</th>
-    <th>CPF</th>
-    <th>Telefone 1</th>
-    <th>Telefone 2</th>
-    <th>Cidade</th>
-    <th>Rua</th>
-    <th>Numero</th>
-  </tr>
-  <tr>
-    <td>$linha['alunonome']</td>
-    <td>$linha['cursonome']</td>
-    <td>$tel1</td>
-    <td>$tel2</td>
-    <td>$cidade</td>
-    <td>$rua</td>
-    <td>$numero</td>
-  </tr>
-</table>
-
-<h3>Histórico de Locações de Fita</h3>
-
-<table>
-  <tr>
-    <th>Nome do Cliente</th>
-    <th>Filme Locado</th>
-    <th>Valor da Locação</th>
-    <th>Multa</th>
-    <th>Data da Locação</th>
-    <th>Data de Entrega</th>
-  </tr>
-  <tr>
-    <td>$cliente</td>
-    <td>$filme</td>
-    <td>$valor</td>
-    <td>$multa</td>
-    <td>$dataL</td>
-    <td>$dataE</td>
-  </tr>
-</table>
-</body>
-</html>
-";
-
-$arquivo ='vintage.pdf';
-$pdf = new mPDF();
-$css = file_get_contents('css/historico-estilo.css');
-$pdf->WriteHTML($css, 1);
-$pdf->WriteHTML($pagina);
-$pdf->Output($arquivo, 'I');
+ exit;
 
 ?>
-			
-	
-
-
-	
